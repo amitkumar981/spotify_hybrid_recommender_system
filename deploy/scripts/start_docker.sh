b713de35a07bb8ef44c02be790ccceb38a89f5aa
amit redhu <redhus341@gmail.com>
2025-04-28 17:59:04 +0530

    rename staer_docker.sh

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -2,8 +2,7 @@
 # Log everything to start_docker.log
 exec > /home/ubuntu/start_docker.log 2>&1
 
-# Export CI_TOKEN (set your token here)
-export CI_TOKEN=94f5f599e01d773416c043c7b7e819b789f3b7b8
+
 
 echo "Logging in to ECR..."
  aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 565393027942.dkr.ecr.ap-southeast-2.amazonaws.com
