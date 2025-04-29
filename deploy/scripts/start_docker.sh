cba909f4d30ff6014a7b1acc0fd16465589051d2
amit redhu <redhus341@gmail.com>
2025-04-29 15:25:40 +0530

    update docker.sh

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -2,27 +2,25 @@
 # Log everything to start_docker.log
 exec > /home/ubuntu/start_docker.log 2>&1
 
-
-
 echo "Logging in to ECR..."
- aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 565393027942.dkr.ecr.ap-southeast-2.amazonaws.com
+aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 565393027942.dkr.ecr.ap-southeast-2.amazonaws.com
 
 echo "Pulling Docker image..."
-docker push 565393027942.dkr.ecr.ap-southeast-2.amazonaws.com/spotify-hybrid-recommender-system:latest
+docker pull 565393027942.dkr.ecr.ap-southeast-2.amazonaws.com/spotify-hybrid-recommender-system:latest
 
 echo "Checking for existing container..."
-if [ "$(docker ps -q -f name=spotify-hybrid-recommender-system)"]; then
+if [ "$(docker ps -q -f name=spotify-hybrid-recommender-system)" ]; then
     echo "Stopping existing container..."
-    docker stop swiggy-food-delivery-time-prediction
+    docker stop spotify-hybrid-recommender-system
 fi
 
-if [ "$(docker ps -aq -f name=spotify-hybrid-recommender-system)"];then
+if [ "$(docker ps -aq -f name=spotify-hybrid-recommender-system)" ]; then
     echo "Removing existing container..."
     docker rm spotify-hybrid-recommender-system
 fi
 
 echo "Starting new container..."
 docker run -d -p 80:8000 --name spotify-hybrid-recommender-system \
- 
+565393027942.dkr.ecr.ap-southeast-2.amazonaws.com/spotify-hybrid-recommender-system:latest
 
 echo "Container started successfully."
